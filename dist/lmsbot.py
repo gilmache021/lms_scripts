e7c46a4e7f7b764c60b89a2efb34c0b031c6b77d
lifeoncode <ndu918@outlook.com>
2021-12-07 17:21:20 +0200

    fix: main script more efficient in python

--- dist/lmsbot.py
+++ dist/lmsbot.py
@@ -1,7 +1,17 @@
-
+import subprocess
 # GREET USER
 
 print("Hello friend, I'm your wtc-lms assistant.")
 while True:
-    todo = input("What can I assist with? ")
+    todo = input("What can I assist with? ").lower()
+    if todo == "login":
+        subprocess.run(["lmslogin"])
     
+    elif todo == "review":
+        subprocess.run(["review"])
+
+    elif todo == "details":
+        subprocess.run(["details"])
+    
+    else:
+        subprocess.run(["echo", "Did not understand..."])
